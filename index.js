dac7ef2a998398c2e72e850a1c915a78c2a84593
Gabriel Frink <frinkdotdesign@gmail.com>
2018-09-06 12:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 
 var name = "Janet"
-var height = "74"
+var height = 74
 
-console.log(parseInt(height) + 1)
+console.log(height + 1)
 console.log(name)
 
 console.log("Height:")
